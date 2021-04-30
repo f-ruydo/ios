21292707a47c13dbfddd9d3adebd901da48e4af1
Enrique Sandoval <enrique.sandoval@ruydo.com>
2021-04-30 09:28:45 -0400

    Initial Commit

---
+++ VRSN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VRSN
+//
+//  Created by Enrique on 30-04-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ VRSN/VRSNApp.swift
@@ -0,0 +1,17 @@
+//
+//  VRSNApp.swift
+//  VRSN
+//
+//  Created by Enrique on 30-04-21.
+//
+
+import SwiftUI
+
+@main
+struct VRSNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
